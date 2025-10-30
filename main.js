9c9666910c82a5658286780cb3d58e8e78e2fb5a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-29 22:16:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+
+
+
+function cifrarAtbash(texto) {
+    let alfaMaiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let alfaMinusculo = "abcdefghijklmnopqrstuvwxyz";
+    let saida = "";
+
+    for (let i = 0; i < texto.length; i++) {
+        let char = texto[i];
+        let indice, indiceInvertido;
+
+        if (alfaMaiusculo.indexOf(char) !== -1) {
+            indice = alfaMaiusculo.indexOf(char);
+            indiceInvertido = 25 - indice;
+            saida += alfaMaiusculo[indiceInvertido];
+        } else if (alfaMinusculo.indexOf(char) !== -1) {
+            indice = alfaMinusculo.indexOf(char);
+            indiceInvertido = 25 - indice;
+            saida += alfaMinusculo[indiceInvertido];
+        } else {
+            saida += char;
+        }
+    }
+
+    return saida;
+}
+
+
+function cifrarCesar(mensagem, chave) {
+  let resultado = "";
+
+  for (let i = 0; i < mensagem.length; i++) {
+    let char = mensagem[i];
+
+    if (char >= 'A' && char <= 'Z') {
+      let codigo = ((char.charCodeAt(0) - 65 + chave + 26) % 26) + 65;
+      resultado += String.fromCharCode(codigo);
+    }
+    else if (char >= 'a' && char <= 'z') {
+      let codigo = ((char.charCodeAt(0) - 97 + chave + 26) % 26) + 97;
+      resultado += String.fromCharCode(codigo);
+    }
+    else {
+      resultado += char;
+    }
+  }
+
+  return resultado;
+}
+
+
+
+
+function cifrarVigenere(mensagem, palavraChave, modo = 'codificar') {
+    let resultado = "";
+    let chaveIndex = 0;  
+    let chave = palavraChave.toUpperCase();  
+
+    for (let i = 0; i < mensagem.length; i++) {
+        let char = mensagem[i];
+
+
+        if (char >= 'A' && char <= 'Z') {
+            let deslocamento = chave.charCodeAt(chaveIndex % chave.length) - 65; 
+            if (modo === 'decodificar') {
+                deslocamento = -deslocamento;
+            }
+
+            let novoCodigo = (char.charCodeAt(0) - 65 + deslocamento + 26) % 26 + 65;
+            resultado += String.fromCharCode(novoCodigo);
+
+            chaveIndex++;  
+        }
+        
+        else if (char >= 'a' && char <= 'z') {
+            let deslocamento = chave.charCodeAt(chaveIndex % chave.length) - 65;
+            if (modo === 'decodificar') {
+                deslocamento = -deslocamento;  
+            }
+
+            let novoCodigo = (char.charCodeAt(0) - 97 + deslocamento + 26) % 26 + 97;
+            resultado += String.fromCharCode(novoCodigo);
+
+            chaveIndex++; 
+        }
+        else {
+            resultado += char;
+        }
+    }
+
+    return resultado;
+}
+
